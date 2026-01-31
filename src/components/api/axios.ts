ad24755fae7af331d85b85cd552a7fc7dac35e22
rolex-marker <hjiajian95@gmail.com>
2026-01-30 18:18:16 -0800

    chore: changenetwork 7

--- src/components/api/axios.ts
+++ src/components/api/axios.ts
@@ -4,5 +4,8 @@ const API_BASE = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: API_BASE,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    "bypass-tunnel-reminder": "true" // This tells Pinggy to skip the warning page
+  }
 });
